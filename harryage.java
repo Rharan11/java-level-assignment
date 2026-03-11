eebff81003743ecd33a43aeabdb42db6cf97ce41
Rharan11 <rh9278@srmist.edu.in>
2026-03-11 11:08:53 +0530

    Create harryage.java

---
+++ harryage.java
@@ -0,0 +1,16 @@
+// Program to calculate Harry's age in 2024
+class HarryAge {
+    public static void main(String[] args) {
+
+        // Creating variables
+        String name = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculating age
+        int age = currentYear - birthYear;
+
+        // Display result
+        System.out.println(name + "'s age in " + currentYear + " is " + age);
+    }
+}
